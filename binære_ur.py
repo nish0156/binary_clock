082a11818356711f03ca5d3f59aa36270e8f5cce
nish0156 <nish0156@elev.tec.dk>
2023-08-23 14:28:07 +0200

    søjler lodret added

--- binære_ur.py
+++ binære_ur.py
@@ -13,6 +13,10 @@ off = (0, 0, 0)
 
 hat.clear()
 
+hat.show_message("Programmet Starter", scroll_speed=0.05)
+
+hat.clear()
+
 def display_binary(value, row, color):
     binary_str = "{0:8b}".format(value)
     for x in range(0, 8):
@@ -21,12 +25,45 @@ def display_binary(value, row, color):
         else:
             hat.set_pixel(x, row, off)
 
-while True:
+
+
+def vandret():
+ while True:
     t = datetime.datetime.now()
     display_binary(t.hour, 3, hour_color)
     display_binary(t.minute, 4, minute_color)
     display_binary(t.second, 5, second_color)
     time.sleep(0.0001)
+
+
+
+def display_vertical(value, row, color):
+    binary_str = "{0:8b}".format(value)
+    for y in range(0, 8):
+        if binary_str[y] == '1':
+            hat.set_pixel(row, y,color)
+        else:
+            hat.set_pixel(row, y,off)
+
+def lodret():
+ while True:
+    t = datetime.datetime.now()
+    display_vertical((int(t.hour//10)), 2, hour_color)
+    display_vertical((int((t.hour%10)//1)), 3, hour_color)
+    display_vertical((int(t.minute//10)), 4, minute_color)
+    display_vertical((int((t.minute%10)//1)), 5, minute_color)
+    display_vertical((int(t.second//10)), 6, second_color)
+    display_vertical((int((t.second%10)//1)), 7, second_color)
+    time.sleep(0.0001)
+   
+hat.clear()
+
+lodret()
+hat.clear()
+
+
+
+
 
 
 
